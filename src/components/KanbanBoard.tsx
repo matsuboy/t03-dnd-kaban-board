5ce4f136249196f75f71474f23abe88098d199c4
matsuboy <matsuboy.dev@gmail.com>
2025-01-01 18:17:11 -0300

    add column title

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,21 +1,36 @@
+import { useState } from 'react'
 import PlusIcon from '../icons/PlusIcon'
+import { Column } from '../types'
 
 export function KanbanBoard() {
+  const [columns, setColumns] = useState<Column[]>([])
+
+  console.log(columns)
+
   return (
     <div
       className="
-                  m-auto
-                  flex
-                  min-h-screen	
-                  w-full
-                  items-center
-                  overflow-x-auto
-                  overflow-y-hidden
-                  px-[40px]
+              m-auto
+              flex
+              min-h-screen	
+              w-full
+              items-center
+              overflow-x-auto
+              overflow-y-hidden
+              px-[40px]
     "
     >
       <div className="m-auto">
+        <div>
+          {columns.map((column) => (
+            <div>{column.title}</div>
+          ))}
+        </div>
+
         <button
+          onClick={() => {
+            createNewColumn()
+          }}
           className="
                   h-[60px]
                   w-[350px]
@@ -39,4 +54,17 @@ export function KanbanBoard() {
       </div>
     </div>
   )
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    }
+    setColumns([...columns, columnToAdd])
+  }
+}
+
+function generateId() {
+  /* Generate a random number between 0 and 10_000 */
+  return Math.floor(Math.random() * 10_001)
 }
